14c2b05fcb33269166d743deb4480bdedf99a3de
xixinmiaoxi <740401977@qq.com>
2017-08-14 18:05:54 +0800

    UI

--- Assets/Scripts/UI/OnReceived.cs
+++ Assets/Scripts/UI/OnReceived.cs
@@ -8,23 +8,28 @@ using System;
 
 public class OnReceived : MonoBehaviour
 {
-    List<Node> Curpartlist = new List<Node>();
-
-    private int CurClassNum = 0;
-    private int UI_Btn_Num = 12;
-    private int CurPartPage = 1;
-
-    private List<GameObject> lastlist = new List<GameObject>();
-    private List<Node> mynodes = new List<Node>();
-    private List<string> mytypes = new List<string>();
+    public List<Node> Curpartlist = new List<Node>();
 
     //声明字典maps
     public Dictionary<MyTransform, List<Node>> maps = new Dictionary<MyTransform, List<Node>>();
 
     public UIPartsClassPanel partsclass;
-    public Transform parent;
+    public Transform parent;//就是SinglePartPanel，即该脚本所挂物体
 
-    public Button NextPage, PreviousPage;
+    public Button NextPage, PreviousPage;//分别表示下一页，上一页按钮
+    public GameObject ViewPage;//显示当前类别下第几页，共几页
+
+    private int CurClassNum = 1;//当前页码 （属零件类的翻页，左右按钮，每三个类别一翻页）
+    private int UI_Btn_Num = 12;//零件架每页显示零件的个顺
+    private int CurPartPage = 1;//当前页数（当前类别下的多个零件翻页，上一页，下一页按钮，每12个零件一翻页）
+
+
+    //上一刷新的page
+    private int lastpage = -1;
+
+    private List<GameObject> lastlist = new List<GameObject>();
+    private List<Node> mynodes = new List<Node>();
+    private List<string> mytypes = new List<string>();
 
     // Use this for initialization
     void Start()
@@ -33,7 +38,7 @@ public class OnReceived : MonoBehaviour
         mynodes = common.GetNodeList();
         mytypes = common.GetNodeTypes();
         
-        CurClassNum = 1;
+        //CurClassNum = 1;
 
         //将零件类型和其所包含的零件集合分别作为字典的Key和Value添加到字典maps
         InitNodes();
@@ -50,6 +55,22 @@ public class OnReceived : MonoBehaviour
     void NextPage_Btn()
     {
         int PartTotalPage = Mathf.CeilToInt(Curpartlist.Count / (float)UI_Btn_Num);
+        //if (PartTotalPage <= 0)
+        //{
+        //    return;
+        //}
+
+        //if (CurPartPage >= PartTotalPage)
+        //{
+        //    return;
+        //}
+
+        //CurPartPage += 1;
+
+        //if (CurPartPage >= PartTotalPage)
+        //{
+        //    CurPartPage = PartTotalPage;
+        //}
         if (CurPartPage < PartTotalPage && CurClassNum > 0)
         {
             CurPartPage++;
@@ -79,7 +100,16 @@ public class OnReceived : MonoBehaviour
     //刷新
     private void OnCallBack(int obj)
     {
+        //重复点击返回
+        if (lastpage == obj )
+        {
+            return;
+        }
+        lastpage = obj;
+
         Cleared(lastlist);
+
+       // RefreshToogles(obj);
 
         List<MyTransform> mys = new List<MyTransform>(maps.Keys);
 
@@ -95,18 +125,40 @@ public class OnReceived : MonoBehaviour
 
                 //默认刷新第一页
                 CurPartPage = 1;
-                RefreshPage(CurPartPage);
 
+                RefreshIndex(CurPartPage);
+
+                RefreshPage(CurPartPage);
             }
         }
     }
 
+
+    //获取当前总页数（小）
+    private int GetCurrentIndex()
+    {
+        int count = Curpartlist.Count;
+        int PartTotalPage = Mathf.CeilToInt(count / (float)UI_Btn_Num);
+        return PartTotalPage;
+    }
+
+    //刷新按钮
+    private void RefreshIndex(int first)
+    {
+        ViewPage.GetComponent<Text>().text = "第"+first+"页/共"+ GetCurrentIndex() + "页";
+    }
+
+
     void RefreshPage(int CurPartPage)
     {
+        //刷新显示
+        RefreshIndex(CurPartPage);
+
+        //
         List<Node> temp = new List<Node>();
         int count = Curpartlist.Count;
         int PartTotalPage = Mathf.CeilToInt(count / (float)UI_Btn_Num);
-        if (CurPartPage >= 1)
+        if (CurPartPage > 1)
         {
             Cleared(lastlist);
             //如果不是最后一页，遍历属于当前类型的的零件并记录在集合
@@ -119,7 +171,7 @@ public class OnReceived : MonoBehaviour
             }
             else
             {
-                for (int i = CurPartPage * (UI_Btn_Num-1); i < count; i++)
+                for (int i = UI_Btn_Num * (CurPartPage - 1); i < count; i++)
                 {
                     temp.Add(Curpartlist[i]);
                 }
@@ -127,7 +179,7 @@ public class OnReceived : MonoBehaviour
         }
         else
         {
-            if (count / UI_Btn_Num <= 1)
+            if (count / (float)UI_Btn_Num <= 1)
             {
                 for (int i = 0; i < count; i++)
                 {
@@ -151,14 +203,17 @@ public class OnReceived : MonoBehaviour
             string name = partOwnCurType.partName;
             string type = partOwnCurType.Type;
             GameObject go = partOwnCurType.gameObject;
+            Text t = parent.GetChild(i).GetChild(0).GetComponent<Text>();
+            t.gameObject.SetActive(true);
+            t.text =  name + "  " + type;
+
             if (go.GetComponent<MeshRenderer>())
             {
                 go.GetComponent<MeshRenderer>().enabled = true;
 
             }
-
             go.transform.SetParent(tran);//设置零件位置等于cube的位置
-            go.transform.localScale = Vector3.one* 0.2f;//GlobalVar.ModelSize;
+            go.transform.localScale = Vector3.one * 0.2f;
             go.transform.localPosition = Vector3.zero;
             lastlist.Add(go);
         }
@@ -167,6 +222,14 @@ public class OnReceived : MonoBehaviour
 
     void Cleared(List<GameObject> temp)
     {
+        //将Text全部隐藏
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Text t = parent.GetChild(i).GetChild(0).GetComponent<Text>();
+            t.gameObject.SetActive(false);
+        }
+
+
         for (int i = 0; i < temp.Count; i++)
         {
             GameObject go = temp[i];
--- Assets/Scripts/UI/UIPartsClassPanel.cs
+++ Assets/Scripts/UI/UIPartsClassPanel.cs
@@ -8,6 +8,9 @@ using System;
 
 public class UIPartsClassPanel : MonoBehaviour
 {
+    [SerializeField]
+    private Toggle PartClass1, PartClass2, PartClass3;
+
     private Action<int> call;
     private int CurClassNum = 0;
 
@@ -24,14 +27,18 @@ public class UIPartsClassPanel : MonoBehaviour
     /// <param name="callback"></param>
     public void Init(Dictionary<MyTransform, List<Node>> map , Action<int> callback)
     {
+
         call = callback;
         maps = map;
         mystyps = new List<MyTransform>(map.Keys);
 
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            transform.GetChild(i).GetComponentInChildren<Text>().text = mystyps[i].partclassname;
-        }
+        //for (int i = 0; i < transform.childCount; i++)
+        //{
+        //    transform.GetChild(i).GetComponentInChildren<Text>().text = mystyps[i].partclassname;
+        //}
+
+        UpdateListener(1);
+
     }
 
     void Start()
@@ -41,34 +48,39 @@ public class UIPartsClassPanel : MonoBehaviour
         CurClassNum = 1;
         next.onClick.AddListener(Nextbut);
         previous.onClick.AddListener(Previousbut);
+
     }
 
     //右方向
     void Nextbut()
     {
-        if (CurClassNum < mystyps.Count && CurClassNum > 0)
+        int pagestotal = Mathf.CeilToInt( mystyps.Count / 3f);
+        if (CurClassNum < pagestotal && CurClassNum > 0)
         {
-            
             CurClassNum++;
-            Refresh(CurClassNum);
-        }
-        else
-        {
-            Debug.Log("currentpage 最大不能再加了" + CurClassNum);
+            int current = ((CurClassNum - 1 )*3) + 1;
+            if (current <= mystyps.Count)
+            {
+                Refresh(current);
+            }
         }
     }
 
     //左方向
     void Previousbut()
     {
-        if (CurClassNum >1)
+        int pagestotal = Mathf.CeilToInt(mystyps.Count / 3f);
+
+        if (CurClassNum > 1)
         {
             CurClassNum--;
-            Refresh(CurClassNum);
+            int current = ((CurClassNum - 1) * 3) + 1;
+            if (current <= mystyps.Count)
+            {
+                Refresh(current);
+            }
         }
-        else {
-            Debug.Log("currentpage == 1不能再减了");
-        }
+
     }
 
 
@@ -77,8 +89,88 @@ public class UIPartsClassPanel : MonoBehaviour
     {
         if (call != null)
         {
-            call(CurClassNum);
+            //刷1，4，7，11
+            //call(CurClassNum);
+
+
+            UpdateListener(CurClassNum);
         }
+    }
+
+
+    void UpdateListener(int obj)
+    {
+        PartClass1.gameObject.SetActive(true);
+        PartClass2.gameObject.SetActive(true);
+        PartClass3.gameObject.SetActive(true);
+        //移除监听
+        PartClass1.onValueChanged.RemoveAllListeners();
+        PartClass2.onValueChanged.RemoveAllListeners();
+        PartClass3.onValueChanged.RemoveAllListeners();
+
+        //关闭无用Toggle
+        if (obj + 2 > mystyps.Count)
+        {
+            PartClass2.gameObject.SetActive(false);
+            PartClass3.gameObject.SetActive(false);
+
+            PartClass1.GetComponentInChildren<Text>().text = mystyps[obj - 1].partclassname;
+        }
+        else if (obj + 3 > mystyps.Count)
+        {
+            PartClass3.gameObject.SetActive(false);
+
+            PartClass1.GetComponentInChildren<Text>().text = mystyps[obj - 1].partclassname;
+            PartClass2.GetComponentInChildren<Text>().text = mystyps[obj].partclassname;
+        }
+        else {
+
+            PartClass1.GetComponentInChildren<Text>().text = mystyps[obj - 1].partclassname;
+            PartClass2.GetComponentInChildren<Text>().text = mystyps[obj].partclassname;
+            PartClass3.GetComponentInChildren<Text>().text = mystyps[obj + 1].partclassname;
+        }
+
+        //更新监听
+        PartClass1.onValueChanged.AddListener(
+            (Ison) => 
+            {
+                if (call != null && Ison)
+                {
+                    call(obj);
+                }
+            }
+            );
+        PartClass2.onValueChanged.AddListener(
+            (Ison) => 
+            {
+                if (call != null && Ison)
+                {
+                    call(obj + 1);
+                }
+            }
+            );
+        PartClass3.onValueChanged.AddListener(
+            (Ison) => 
+            {
+                if (call != null && Ison)
+                {
+                    call(obj + 2);
+                }
+            }
+            );
+
+
+
+        //刷新默认Toggle的IsOn状态
+        if (PartClass1.isOn)
+        {
+            PartClass1.onValueChanged.Invoke(true);
+        }
+        else {
+            PartClass1.isOn = true;
+        }
+        PartClass2.isOn = false;
+        PartClass3.isOn = false;
     }
 
 }
