928e6ff226dcfe45591efb1c78ba5ac96af6b9d3
zhangqi <1564171310@qq.com>
2017-09-06 14:37:27 +0800

    Delete some script

---
+++ Assets/Scripts/Parts/AssembleModel.cs
@@ -0,0 +1,18 @@
+﻿/// <copyright>(c) 2017 WyzLink Inc. All rights reserved.</copyright>
+/// <author>zq</author>
+/// <summary>
+/// 选择的虚拟拆装模式
+/// </summary>
+
+namespace WyzLink.Parts
+{
+    public enum AssembleModel
+    {
+        DemonstrationModel,                      //演示模式
+
+        StudyModel,                              //学习模式
+
+        ExamModel,                               //考试模式
+    }
+
+}
--- Assets/Scripts/UI/EntryMode.cs
+++ Assets/Scripts/UI/EntryMode.cs
@@ -9,6 +9,7 @@
     public class EntryMode : MonoBehaviour
     {
         public static string Mode = "Study";
+
         private GameObject RootPartGameObject;//LCD1
         private GameObject StartCanvas;//开始界面
         private GameObject Canvas;//工作UI
@@ -20,18 +21,13 @@
         public Button Study;//学习模式
         public Button Test;//考试模式
 
-        AssembleManager assemblemanager;
-
         // Use this for initialization
         void Start()
         {
-            //GlobalVar._TipErrBtn.GetComponent<Button>().onClick.AddListener(ErrBtnOnClick);
             GlobalVar._TipCanvas.SetActive(false);
-            assemblemanager = AssembleManager.Instance;
             RootPartGameObject = FindObjectOfType<AssembleController>().gameObject;
             Canvas = GameObject.Find("Canvas");
             ComeCanvas = GameObject.Find("ComeCanvas");
-            //Restart =  GameObject.Find("Restart").GetComponent<Button>();
             if (null != RootPartGameObject)
             {
                 foreach (Transform tran in RootPartGameObject.transform)
@@ -42,11 +38,9 @@
                 ComeCanvas.SetActive(false);
             }
             StartCanvas = GameObject.Find("StartCanvas");
-            // Study.GetComponent<Button>().onClick.AddListener(StartAssemble);
             Study.GetComponent<Button>().onClick.AddListener(StudyComein);
             demonstration.GetComponent<Button>().onClick.AddListener(demonstrationComein);
             Test.GetComponent<Button>().onClick.AddListener(TestComein);
-            //Debug.Log("assemblemanager == null" + assemblemanager == null);
             Restart.GetComponent<Button>().onClick.AddListener(StartAssemble);
         }
 
@@ -76,8 +70,6 @@
             StartCanvas.SetActive(false);
             ComeCanvas.SetActive(true);
             Mode = "Test";
-
-            //SceneManager.LoadScene(2);
         }
 
         private void StartAssemble()
@@ -91,18 +83,8 @@
             }
             Canvas.SetActive(true);
             ComeCanvas.SetActive(false);
-            Debug.Log("assemblemanager == null" + AssembleManager.Instance.InstalledNode == null);
 
             AssembleManager.Instance.Init();
-        }
-
-        private void ErrBtnOnClick()
-        {
-            //GlobalVar._TipErrBtn.SetActive(false);
-            //if (Mode =="Test")
-            //{
-            //    AssembleManager.Instance.InstalledNode();
-            //}
         }
     }
 }
